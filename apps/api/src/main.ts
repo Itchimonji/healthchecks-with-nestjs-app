2b2c56caf6c586f39eeaa5a2c552e28873c7db30
Patrick <Patrick.Creutzburg@e2m.energy>
2021-06-13 23:04:58 +0200

    chore(): add docker

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
