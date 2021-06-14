31ed750a1dadbd06c5cdb767351b35b26c78bed3
Patrick Creutzburg <sapandsaucy@googlemail.com>
2021-06-14 08:32:03 +0200

    chore(): update dependencies

--- apps/api/src/app/metrics/metrics.service.ts
+++ apps/api/src/app/metrics/metrics.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import {HealthService} from "../health/health.service";
-import {PrometheusService} from "../prometheus/prometheus.service";
+import { HealthService } from "../health/health.service";
+import { PrometheusService } from "../prometheus/prometheus.service";
 
 @Injectable()
 export class MetricsService {
