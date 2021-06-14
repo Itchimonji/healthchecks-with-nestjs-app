49d5228faed270efc82455432ad95fbeb4d13d83
Patrick Creutzburg <sapandsaucy@googlemail.com>
2021-06-14 08:46:24 +0200

    update done

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, CommonModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
